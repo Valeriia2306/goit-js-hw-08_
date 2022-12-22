7b36308d983353c60c9859c4d0a7fda81de47eee
Valeriia Frolova <valeriiats1989@gmail.com>
2022-12-22 16:12:54 +0200

    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+console.log(SimpleLightbox);
+// console.log(galleryItems);
