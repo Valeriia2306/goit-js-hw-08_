33e9b2388bc795f491d9bd196c014426ec1bb5ce
Valeriia Frolova <valeriiats1989@gmail.com>
2022-12-23 15:43:43 +0200

    change2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-console.log(SimpleLightbox);
-// console.log(galleryItems);
+const picturesContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryOfPicturesMarkup(galleryItems);
+picturesContainer.innerHTML = galleryMarkup;
+
+function createGalleryOfPicturesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join(``);
+}
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
